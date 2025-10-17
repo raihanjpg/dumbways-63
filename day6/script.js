425229c32bd06a2673b3554de0ba66867ce7d3f7
raihan <raihanswag24@gmail.com>
2025-10-17 13:32:41 +0700

    do assignments, make a profile card

--- day6/script.js
+++ day6/script.js
@@ -1,29 +1,29 @@
 let projectCard = [
-  { image: "https://a.storyblok.com/f/119877/1440x960/535ae83f94/adobestock_120392352_web.jpg",
-    title: "Dumbways web app -2025",
-    duration: "1 bulan",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" },
+//   { image: "https://a.storyblok.com/f/119877/1440x960/535ae83f94/adobestock_120392352_web.jpg",
+//     title: "Dumbways web app -2025",
+//     duration: "1 bulan",
+//     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" },
   
-    { image: "https://th.bing.com/th?id=OIF.o99iCqhjQcBLqf8MF%2fXKRQ&w=319&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
-    title: "Dumbways web app -2025",
-    duration: "1 bulan",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" },
+//     { image: "https://th.bing.com/th?id=OIF.o99iCqhjQcBLqf8MF%2fXKRQ&w=319&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
+//     title: "Dumbways web app -2025",
+//     duration: "1 bulan",
+//     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" },
   
-    { image: "https://th.bing.com/th/id/OIP.akl_eQWJAPNt2o8yMr--TAHaEK?w=300&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
-    title: "Dumbways web app -2025",
-    duration: "1 bulan",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" },
+//     { image: "https://th.bing.com/th/id/OIP.akl_eQWJAPNt2o8yMr--TAHaEK?w=300&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
+//     title: "Dumbways web app -2025",
+//     duration: "1 bulan",
+//     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" },
 
-    { image: "https://th.bing.com/th/id/OIP.f9uy8Wra5c246RU5F57RngHaE8?w=264&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-    title: "Dumbways web app -2025",
-    duration: "1 bulan",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" },
+//     { image: "https://th.bing.com/th/id/OIP.f9uy8Wra5c246RU5F57RngHaE8?w=264&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+//     title: "Dumbways web app -2025",
+//     duration: "1 bulan",
+//     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" },
   
-    { image: "https://i.pinimg.com/1200x/0b/5a/3f/0b5a3ff8a39162c938bdd87a44cf1d91.jpg",
-    title: "Dumbways web app -2025",
-    duration: "1 bulan",
-    description: "Nokia is a Finnish multinational corporation founded on 12 May 1865 as a single paper mill operation. Through the 19th century the company expanded, branching into several different products. In 1967, the Nokia corporation was formed. In the late 20th century, the company took advantage of the increasing popularity of computer and mobile phones. " }
-];
+//     { image: "https://i.pinimg.com/1200x/0b/5a/3f/0b5a3ff8a39162c938bdd87a44cf1d91.jpg",
+//     title: "Dumbways web app -2025",
+//     duration: "1 bulan",
+//     description: "Nokia is a Finnish multinational corporation founded on 12 May 1865 as a single paper mill operation. Through the 19th century the company expanded, branching into several different products. In 1967, the Nokia corporation was formed. In the late 20th century, the company took advantage of the increasing popularity of computer and mobile phones. " }
+ ];
 
 // Elemen target di HTML
 let container = document.getElementById("card-container");
--- profile-card.js
+++ profile-card.js
@@ -1,38 +1,162 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
+import { Pool } from 'pg';
+import multer from "multer";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+const db = new Pool({
+  user: 'postgres',
+  password: 'admin',
+  host: 'localhost',
+  port: 5432,
+  database: 'b63-personal-web',
+  max: 20
+});
+
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-// setup view engine
+
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "assets", "views"));
+app.set("views", path.join(process.cwd(), "assets", "views"));
 
-// static & form data
-app.use(express.static(path.join(__dirname, "assets")));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("assets"));
+app.use(express.urlencoded({ extended: false }));
 
-// dummy data
-let projects = [];
-
-// routes
-app.get("/", (req, res) => res.render("form", { title: "Home" }));
-app.get("/day6", (req, res) => res.render("form", { title: "Contact" }));
-app.get("/add-project", (req, res) => res.render("aproject", { title: "My Project", projects }));
-
-app.post("/add-project", (req, res) => {
-  const { name, startDate, endDate, description } = req.body;
-  projects.push({ id: projects.length + 1, name, startDate, endDate, description });
-  res.redirect("/myproject");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './assets/uploads')
+  },
+  filename: function (req, file, cb) {
+    
+    cb(null, file.fieldname + Date.now())
+  }
 });
 
-app.get("/project/:id", (req, res) => {
-  const project = projects.find((p) => p.id == req.params.id);
-  res.render("profile", { title: "Detail Project", project });
+const upload = multer({ storage: storage })
+
+
+app.get("/contact", contact);
+app.post("/contact", handleContact);
+app.get("/", home);
+app.get("/login", login)
+app.get("/add-project", addProject)
+app.post("/add-project", upload.single('project') , handleProject)
+app.get("/add-experience", addExperience)
+app.post("/add-experience", handleExperience)
+
+app.get("/card/:id", cardDetail)
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
 });
 
-app.listen(PORT, () => console.log(`Server berjalan di http://localhost:${PORT}`));
+function addExperience(req, res){
+  res.render("add-experience")
+}
+
+function addProject(req, res) {
+  res.render("add-project")
+}
+
+function login(req, res) {
+  res.render("form")
+}
+
+async function home(req, res) {
+   const queryExperience ='SELECT * FROM experiences'
+   const resultExperience = await db.query(queryExperience)
+  
+  const query ='SELECT * FROM project' 
+ const result = await db.query(query)
+
+ console.log(result.rows);
+ res.render('index',{projects:result.rows, experiences:resultExperience.rows})
+}
+
+function contact(req, res) {
+  const phoneNumber = 81281281211;
+  res.render("contact", { phoneNumber });
+}
+
+let accounts = [];
+
+async function handleExperience(req, res) {
+  let { name, company_name, description, technologies, date } = req.body
+  console.log(name, company_name, description, technologies, date);
+
+  const query = `
+  INSERT INTO public."experiences" (name, company_name, description, technologies, date)
+  VALUES ($1, $2, $3, $4, $5)
+  RETURNING *;
+`;
+
+const values = [name, company_name, description, technologies, date];
+
+try{
+const result = await db.query(query, values);
+    console.log('Insert successful:', result.rows[0]);
+    console.log(result.rows);
+    res.redirect("/")
+  } catch (error) {
+    console.error('Error executing query:', error);
+  }
+}
+
+async function handleProject(req, res) {
+  let { name, startDate, endDate, description, technologies, } = req.body
+  console.log(name, startDate, endDate, description, technologies);
+
+  const query = `
+  INSERT INTO public."project" (name, start_date, end_date, description, technologies) 
+  VALUES ($1, $2, $3, $4, $5)
+  RETURNING *;
+`;
+
+  const values = [name, startDate, endDate, description, technologies];
+  
+  try {
+    
+    const result = await db.query(query, values);
+    console.log('Insert successful:', result.rows[0]);
+    console.log(result.rows);
+    res.redirect("/")
+  } catch (error) {
+    console.error('Error executing query:', error);
+  }
+
+}
+
+async function handleContact(req, res) {
+  // let name = req.body.name;
+  // let password = req.body.password;
+  let { email, password } = req.body
+
+  console.log(email, password);
+
+
+  let account = {
+    email,
+    password,
+  };
+
+  const query = `SELECT * FROM login`
+  const result = await db.query(query);
+  console.log(result.rows);
+
+
+
+
+  // accounts.push(account);
+  // console.log(accounts);
+
+
+}
+
+
+function cardDetail(req, res) {
+  console.log(req.params.id);
+
+}
+
